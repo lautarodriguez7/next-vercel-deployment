dc95605b21a41a6bb36d9bce025746f9d33ceabb
Lautaro Rodriguez <lautarorodriguez@MacBook-Air-de-Lautaro.local>
2023-08-29 10:30:41 -0300

    adding docker

--- next.config.js
+++ next.config.js
@@ -3,4 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+module.exports = {
+  reactStrictMode: true,
+  experimental: {
+    outputStandalone: true,
+  }
+}
